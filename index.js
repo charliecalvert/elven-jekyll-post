ae6dabc3746376a250c930b728cedde1cb7fd4d9
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-05 18:11:57 -0700

    buildFrintmatter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import { createJekyllPost } from "./utils.js";
 import { input } from '@inquirer/prompts';
-
+import { buildFrontMatter, createPostDateFileName } from "./create-post-date.js";
 
 async function run() {
     console.log("index run called");
@@ -11,3 +11,4 @@ async function run() {
 }
 
 export default run;
+export { buildFrontMatter, createPostDateFileName };
